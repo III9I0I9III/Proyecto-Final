4fa9650f33b868e421974eea3544af0855eeec82
Armando <doc.code.html@gmail.com>
2025-05-23 12:17:56 -0600

    actualiazacion de pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Tailwind debe estar configurado y enlazado aquí
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
